f83b8ec4fc6e8ed13b2bcdfcf9e2d49e934ef773
Chris <chris.ware.dev@gmail.com>
2021-08-21 23:23:09 -0700

    Initial Setup

---
+++ src/Review.js
@@ -0,0 +1,13 @@
+import React from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+
+const Review = () => {
+  return (
+    <div>
+      <h2>Review Component</h2>
+    </div>
+  );
+};
+
+export default Review;
